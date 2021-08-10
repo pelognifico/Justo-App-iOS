9d18f5820d70ba8bb1723b911293933f093fa731
Carlos Villamizar <>
2021-08-10 12:25:46 -0400

    fixing .gitignore

--- Justo App/Pods/Target Support Files/Pods-Justo App-Justo AppUITests/Pods-Justo App-Justo AppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Justo_App_Justo_AppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Justo_App_Justo_AppUITests
-@end
--- Justo App/Pods/Target Support Files/Pods-Justo AppTests/Pods-Justo AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Justo_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Justo_AppTests
-@end
